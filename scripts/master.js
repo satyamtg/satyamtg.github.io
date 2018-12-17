f93fc7d58344fad271fd9ab6ca042a8a8b57ed87
Satyam Kumar <io.satyamtg@gmail.com>
2018-12-17 12:11:02 +0530

    Initialise website

---
+++ scripts/master.js
@@ -0,0 +1,26 @@
+
+//Start the timer
+var countdownTimer = setInterval(updateClock, 1000);
+
+//Function to compute time
+function updateClock() {
+  var deadline = new Date("Jan 1, 2019 00:00:00").getTime();
+  var now = new Date().getTime();
+  var diff = deadline - now;
+  var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((diff%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  document.getElementById("day").innerHTML =days ;
+  document.getElementById("hour").innerHTML =hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML =seconds;
+  if (diff < 0) {
+    clearInterval(countdownTimer);
+    document.getElementById("demo").innerHTML = "TIME UP";
+    document.getElementById("day").innerHTML ='0';
+    document.getElementById("hour").innerHTML ='0';
+    document.getElementById("minute").innerHTML ='0' ;
+    document.getElementById("second").innerHTML = '0';
+  }
+}
