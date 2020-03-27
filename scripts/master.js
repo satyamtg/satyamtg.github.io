9f5712c9cc5c8287707bb87f055af4257ccefe98
Satyam Kumar <io.satyamtg@gmail.com>
2020-03-28 01:32:33 +0530

    Update master.js

--- scripts/master.js
+++ scripts/master.js
@@ -4,7 +4,7 @@ var countdownTimer = setInterval(updateClock, 1000);
 
 //Function to compute time
 function updateClock() {
-  var deadline = new Date("Mar 16, 2020 00:00:00").getTime();
+  var deadline = new Date("Apr 16, 2020 00:00:00").getTime();
   var now = new Date().getTime();
   var diff = deadline - now;
   var days = Math.floor(diff / (1000 * 60 * 60 * 24));
